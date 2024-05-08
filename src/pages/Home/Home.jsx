6d56a7d84a1395c2fe1685c4981d61fafff22622
Babar78 <muhammadbabarashraf8@gmail.com>
2024-05-08 13:44:08 +0500

    feat: Add OurMission section to Home page

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,12 +1,14 @@
 import React from 'react'
 import HeroSection from '../../sections/Home/HeroSection/HeroSection'
 import Footer from '../../components/Footer/Footer'
+import OurMission from '../../sections/Home/OurMission/OurMission'
 
 const Home = () => {
     return (
         <>
-            <div className='content_container'>
+            <div className=''>
                 <HeroSection />
+                <OurMission />
             </div>
             <Footer />
         </>
