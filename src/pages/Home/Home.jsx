3f7c93ce27361d5cedf7f862be328c6588d1db84
Babar78 <muhammadbabarashraf8@gmail.com>
2024-05-08 10:12:18 +0500

    feat: Add Footer component and update Home page layout

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,11 +1,15 @@
 import React from 'react'
 import HeroSection from '../../sections/Home/HeroSection/HeroSection'
+import Footer from '../../components/Footer/Footer'
 
 const Home = () => {
     return (
-        <div className='content_container'>
-            <HeroSection />
-        </div>
+        <>
+            <div className='content_container'>
+                <HeroSection />
+            </div>
+            <Footer />
+        </>
     )
 }
 
